03b19ab1a2b132c3c133433ee1f63104d8880d76
suganthanraj <suganthanraj@gmail.com>
2023-04-25 16:56:01 +0530

    created a separate folder for product

--- mpio/init.py
+++ mpio/init.py
@@ -1,89 +1,61 @@
-import os
-import sys
 import docker
-
-image_names = sys.argv[1]
-# # Set up Docker client
-# client = docker.from_env()
-
-# # Define image name and tag
-# image_name = 'quay.io/tamiltutera/django_mysql_app'
-# image_tag = 'latest'
-
-# # Pull image
-# client.images.pull(image_name, tag=image_tag)
-
-# # Set up environment variables from .env file
-# env_vars = {}
-# with open('.env', 'r') as f:
-#     for line in f:
-#         key, value = line.strip().split('=')
-#         env_vars[key] = value
-# # Define port to expose
-# port_bindings = {8000: 8001}
-
-# # Run container with environment variables
-# container = client.containers.run(
-#     image_name + ':' + image_tag,
-#     name='djang_web_app',
-#     environment=env_vars,
-#     ports=port_bindings,
-#     detach=True
-# )
-
-# # Print container ID
-# print('Container ID:', container.id)
-
-
+import os
+import argparse
 
 # Set up Docker client
 client = docker.from_env()
 
-# login_result = client.login(
-#     registry='quay.io',
-#     username='your-quay.io-username',
-#     password='your-quay.io-password'
-# )
 
-# # Check if login was successful
-# if 'Status' in login_result and login_result['Status'] == 'Login Succeeded':
-#     print('Quay.io login successful')
-#     if client.ping():
-#         print('Docker daemon is responsive')
-#     else:
-#         print('Docker daemon is not responsive')
-# else:
-#     print('Quay.io login failed')
+login_result = client.login(
+    registry='quay.io',
+    username='tamiltutera',
+    password='6VrKr9pSkrzKypnUepGjYbCmbMupMwsXqnp/ONYaz8dcRyatsPcnntTDXemPO4mg'
+)
 
-# Define image names and tags
-image_tags = {
-    'pure_db': 'quay.io/tamiltutera/django_mysql:latest',
-    'pure_web_app': 'quay.io/tamiltutera/django_web_app:latest'
-}
+# Check if login was successful
+if 'Status' in login_result and login_result['Status'] == 'Login Succeeded':
+    print('Quay.io login successful')
+    if client.ping():
+        print('Docker daemon is responsive')
+    else:
+        print('Docker daemon is not responsive')
+else:
+    print('Quay.io login failed')
+
+# Define command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('images', nargs='+', help='list of image names to pull and run')
+args = parser.parse_args()
 
 # Define port bindings
 port_bindings = {
-    'pure_db': {3306: 3306},
-    'pure_web_app': {8000:8001}  # no ports to bind for this image
+    'django_mysql': {3306: 3306},
+    'django_web_app': {8000:8005}  # no ports to bind for this image
 }
 
 # Pull images
-for image_name, image_tag in image_tags.items():
+# for image_name, image_tag in image_tags.items():
     
+#     client.images.pull(image_tag)
+# Pull images
+for image_name in args.images:
+    image_tag = f'quay.io/tamiltutera/{image_name}:latest'
     client.images.pull(image_tag)
 
+
 # Set up environment variables from .env file
-env_file = os.path.join(os.getcwd(), '.env')
 env_vars = {}
-with open(env_file, 'r') as f:
+with open('.env', 'r') as f:
     for line in f:
         key, value = line.strip().split('=')
         env_vars[key] = value
 
 # Run containers with environment variables
 containers = {}
-for image_name, image_tag in image_tags.items():
+for image_name in args.images:
+    
     if image_name in port_bindings:
+    
         container = client.containers.run(
             image_tag,
             environment=env_vars,
@@ -92,6 +64,7 @@ for image_name, image_tag in image_tags.items():
             detach=True
         )
     else:
+        print("ENV not set")
         container = client.containers.run(
             image_tag,
             name=image_name,
